1083a032ca79484fe457e1f3d983884e93da1fc5
Otavio Martins <otaviovacari@usp.br>
2023-05-06 17:22:43 -0300

    add mockdata

--- next-frontend/components/PollCard.tsx
+++ next-frontend/components/PollCard.tsx
@@ -1,15 +1,41 @@
-import React from 'react'
+import React from 'react';
 
-const PollCard = () => {
+const UserPage: React.FC = () => {
+  const mockStakingData = [
+    {
+      user_id: 1,
+      name: 'Alice',
+      email: 'alice@example.com',
+      staking_amount: 1000,
+      pool_share: 0.1,
+      rewards: 50,
+    },
+    {
+      user_id: 2,
+      name: 'Bob',
+      email: 'bob@example.com',
+      staking_amount: 2000,
+      pool_share: 0.2,
+      rewards: 100,
+    },
+  ];
+
   return (
-    <div className='grid-container'>
-      <div className="poll-card">
-          <h1>Yoooo this is a cool polls!</h1>
-          <h2><b>Title</b></h2>
-          <h2><b>Description</b></h2>
+    <div>
+      <h1>Users</h1>
+      <div className="staking-cards">
+        {mockStakingData.map((staking, index) => (
+          <div key={index} className="staking-card">
+            <h3>{staking.name}</h3>
+            <p>Email: {staking.email}</p>
+            <p>Staking Amount: {staking.staking_amount}</p>
+            <p>Pool Share: {staking.pool_share * 100}%</p>
+            <p>Rewards: {staking.rewards}</p>
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PollCard
+export default UserPage;
--- next-frontend/components/UserPage.tsx
+++ next-frontend/components/UserPage.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
 
 const UserPage: React.FC = () => {
+  const mockWalletData = [
+    {
+      walletAddress: '0x12345abcde...',
+      stakedAmount: 120,
+      stakingPool: 'Lido',
+      rewardAmount: 3.5,
+    },
+    {
+      walletAddress: '0x67890fghij...',
+      stakedAmount: 50,
+      stakingPool: 'Lido',
+      rewardAmount: 1.2,
+    },
+  ];
+
   return (
     <div>
       <h1>Users</h1>
+      <div className="wallet-cards">
+        {mockWalletData.map((wallet, index) => (
+          <div key={index} className="wallet-card">
+            <h3>Wallet Address</h3>
+            <p>{wallet.walletAddress}</p>
+            <h3>Staked Amount</h3>
+            <p>{wallet.stakedAmount}</p>
+            <h3>Staking Pool</h3>
+            <p>{wallet.stakingPool}</p>
+            <h3>Reward Amount</h3>
+            <p>{wallet.rewardAmount}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
